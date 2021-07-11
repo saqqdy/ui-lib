fd5b2da0e6509ac69359ab0209d547fbfedc8be3
saqqdy <saqqdy@qq.com>
2021-07-11 13:13:49 +0800

    demo

--- script/deploy-docs.sh
+++ script/deploy-docs.sh
@@ -3,11 +3,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+cd docs
 # 生成静态文件
 yarn run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'github.saqqdy.com' > CNAME
@@ -23,7 +24,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:saqqdy/gitmars.git master:gh-pages
-git push -f git@gitee.com:saqqdy/gitmars.git master:gh-pages
+git push -f git@github.com:saqqdy/ui-lib-demo.git master:gh-pages
 
 cd -
+cd ..
